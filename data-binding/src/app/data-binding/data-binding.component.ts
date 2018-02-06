745adc9bd745c6f071c1ab59eb3aeaa8cb623475
Matheus Barone <matheusvbarone@gmail.com>
2018-02-06 10:01:01 -0200

    First Commit

---
+++ data-binding/src/app/data-binding/data-binding.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-data-binding',
+  templateUrl: './data-binding.component.html',
+  styleUrls: ['./data-binding.component.css']
+})
+export class DataBindingComponent implements OnInit {
+
+  url: string = 'http://loiane.com';
+  cursoAngular: boolean = true;
+  urlImagem: string = 'https://loremflickr.com/320/240/dog';
+
+  getValor(){
+    return 1;
+  }
+
+  getCurtirCurso(){
+    return true;
+  }
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ primeiro-projeto/src/app/meu-primeiro2/meu-primeiro2.component.ts
@@ -0,0 +1,7 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-meu-primeiro2',
+  templateUrl: './meu-primeiro2.component.html'
+})
+export class MeuPrimeiro2Component {}
