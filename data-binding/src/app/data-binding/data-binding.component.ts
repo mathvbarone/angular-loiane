f0afec54d0cbc414f1eea897a1b21454e3e862c8
Igor Abreu Araujo <igor.abreu.araujo@avanade.com>
2018-02-07 15:25:52 -0200

    Event Binding

--- data-binding/src/app/data-binding/data-binding.component.ts
+++ data-binding/src/app/data-binding/data-binding.component.ts
@@ -11,12 +11,33 @@ export class DataBindingComponent implements OnInit {
   cursoAngular: boolean = true;
   urlImagem: string = 'https://loremflickr.com/320/240/dog';
 
+valorAtual: string = '';
+valorSalvo: string='';
+
+isMouseOver: boolean = false;
+
   getValor(){
     return 1;
   }
 
+  botaoClicado(){
+    console.log('Btn clicked');
+  }
+
+  onKeyUp(evento: KeyboardEvent){
+    this.valorAtual = (<HTMLInputElement>evento.target).value;
+  }
+
+  salvarValor(valor){
+    this.valorSalvo = (valor);
+  }
+
   getCurtirCurso(){
     return true;
+  }
+
+  onMouseOverOut(){
+    this.isMouseOver = !this.isMouseOver;
   }
 
   constructor() { }
